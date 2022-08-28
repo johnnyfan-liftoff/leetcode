61b113b89ec3d2c8d7a1abe21075f458cdc748cc
Johnny Fan <oak899@gmail.com>
2022-08-27 20:18:37 -0700

    424, 567, 704

--- 424. Longest Repeating Character Replacement.py
+++ 424. Longest Repeating Character Replacement.py
@@ -4,33 +4,29 @@ def characterReplacement(s, k):
     :type k: int
     :rtype: int
     """
-    maxP = 0
-    h = []
-    l, r = 0, 1
-    while r < len(s):
-        if s[r] != s[l]:
-            if len(h) < k:
-                h.append(r)
-                maxP = max(maxP, r - l + 1)
-            else:
-                h = []
-                l = r
-                while s[r] == s[l-1] and l-1 >= 0 :
-                    l -= 1
-                maxP = max(maxP, r - l + 1)
-        else:
-            maxP = max(maxP, r - l + 1)
-        r += 1
-    return maxP
+    l = 0
+    count = {}
+    res = 0
+    maxf = 0
+
+    for r in range(len(s)):
+        count[s[r]] = count.get(s[r], 0) + 1
+        maxf = max (maxf, count[s[r]])
+        while r - l + 1 - maxf > k:
+            count[s[l]] -= 1
+            l += 1
+
+        res = max(res, r - l + 1)
+    return res
 
 s = "AABABBA"
 k = 1
-# s = "ABAB"
+# s = "ABAB"+
 # k = 2
 # s = "AAAA"
 # k = 2
-s = "ABBB"
-k = 2
+# s = "ABBB"
+# k = 2
 # s = "AABA"
 # k = 0
 print(characterReplacement(s, k))
---
+++ 704. Binary Search.py
@@ -0,0 +1,15 @@
+def search(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
+    l, r = 0, len(nums) - 1
+    while l < r:
+        
+
+
+nums = [-1,0,3,5,9,12]
+target = 9
+
+print (search(nums, target))
